6db26644e11a9f68edb7e019745b50a29b1ed808
josel <joseluisroncoka@hotmail.com>
2026-03-12 15:54:47 -0500

    Separated JS logic into modules, to separate navigation logic and passphrase generation logic. Reimplemented Acronym generation in separate page and updated UI accordingly.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//File: app.js
+//Project: Intro Cybersecurity Project Dice Ware Passphrase Generator. 
+//Author: Jose Ron Coka
+//History: Version 2.0 March 11 2026
+//This should be the main javascript file, handles all the logic of navigation
+//The passphrase generating logic must be moved to a different file and imported here. 
+
+ 
+ 
+ 
  //Setting up global variables. 
       var pLen;
       var numT;
@@ -7,6 +17,9 @@
       const words = []; // array to store the words
       var matchingElements = []; //array to store the matching words to the dice throws
       
+      //Import required functions from generate.js
+      import { generateFunc } from "./generate.js";
+      import { generateAcronym } from "./generate.js";
 
         
       function start() {
@@ -33,84 +46,39 @@
         })
       })
 
-        var genButton = document.getElementById("genBut");
-        genButton.addEventListener("click",generateFunc);
+      
+      //We need to check that the button exists before adding event listener, otherwise it will give an error.
+        const genButton = document.getElementById("genBut");
+        if (genButton){
+          genButton.addEventListener("click",extract);
+        }
+
+      //Listener for Acronym button
+        const genAcrButton = document.getElementById("genAcrBut");
+        if (genAcrButton){
+          genAcrButton.addEventListener("click",extractAcr);
+        }
 
       }
 
-      //Function generate should take length of passphrase generator and call dice that amount of times
-      //get the word from the word list file depending each dice throw 
-      //Can write it into a string array. 
-      //If we are adding symbols and more. if button input call function to change. 
+      
+      
+      //Function Extracts input information and calls the generate function.
+      function extract(){
 
-      function generateFunc(){
-
-
-        //Take string input turn it into char array and compare in function
-        
-
-        //Takes the input from the HTML
+         //Takes the input from the HTML
         numT= document.getElementById("throws").value;
         console.log(numT);
-        var diceArr=new Array(numT);
-
         var pLen= document.getElementById("lenP").value;
-
-        
-
 
         //Prints inputs to the html.
        // document.getElementById("disLen").innerHTML ="Length of Passphrase: "+pLen+" Number of Dice Throws: "+numT;
         console.log(pLen);
-
-        var count=pLen;
-        var passphrase = new Array(pLen);
-
-        
-
-        //Two loops, one runs for every word, another runs the dice roll 6 times for each word.
-        //Two arrays, one for passphrase another for each dice throw.
-        //After second loop runs the array with the dice throws is turned to a string and saved on the passphrase array. 
-        for (i=0; i<pLen;i++)
-        {
-          for (j=0;j<numT;j++)
-          {
-            diceArr[j]=diceRoll();
-            console.log("Element "+(j+1)+" of word "+(i+1)+" is: "+diceArr[j]);
-          }
-          var diceAsString = diceArr.join(''); // Results in a string
-          passphrase[i]=diceAsString; 
-          console.log(passphrase[i]);
-        } 
-
-        console.log(passphrase.length);
-
-
-        //Check which match function to call depending on users input. If no input on Acronym then normal match. 
-        if (initArr.length==0){
-          for (x=0; x<=pLen; x++)
-        {
-          match(passphrase[x]);
-        }
-        }
-        else{
-        for (z=0; z<=pLen; z++){
-          matchInit(passphrase[z],z);
-        }
-        }
-        
-        //Resulting Passphrase is printed 
-        var message="";
-        for (y=0; y<pLen; y++)
-        {
-          message+=matchingElements[y]+ " ";
-        }
+        var message = generateFunc(words,numT,pLen);
 
         //document.getElementById("test").innerHTML ="Passphrase Generated: " + message; 
         document.getElementById("result").innerHTML =message; 
-       
-        //Empty array in case user press button again. 
-        matchingElements=[];
+      
 
         //Final message is printed
         var finalmessage="If you are good with the generated passphrase and wish to use it, "; 
@@ -119,87 +87,40 @@
         finalmessage+="To use it for some accounts you might need to meet requirements like adding numbers and symbols.";
     
         document.getElementById("finMess").innerHTML =finalmessage; 
-
       }
 
-      //Function to generate acronym passphrase
-      function generateAcronym(){
-        //Code for acronym passphrase.
 
+      //Function extracts input information and calls the generate function for acronym passphrase.
+      function extractAcr(){
+
+        //Takes the input from the HTML
+        //Extract required throw number
+        numT= document.getElementById("throws").value;
+        console.log(numT);
+        //Extract word with initials for passphrase
         //document.getElementById("initials").maxlength = pLen;
-        //initL=document.getElementById("initials").value;
-        //initArr=initL.split('');
+        var initL=document.getElementById("initials").value;
+        var initArr=initL.split('');
+        var pLen= initArr.length;
 
+        //Prints inputs to the html.
+       // document.getElementById("disLen").innerHTML ="Length of Passphrase: "+pLen+" Number of Dice Throws: "+numT;
+        console.log(pLen);
+        console.log(initArr);
+        var message = generateAcronym(words,numT,initL);
+
+        //document.getElementById("test").innerHTML ="Passphrase Generated: " + message; 
+        document.getElementById("result").innerHTML =message; 
+      
+
+        //Final message is printed
+        var finalmessage="If you are good with the generated passphrase and wish to use it, "; 
+        finalmessage+="we recommend that you save a copy of it, either on a Password Manager or ";
+        finalmessage+="write it in a piece of paper and store it in a safe place. Then take a little time to try to memorize it.";;
+        finalmessage+="To use it for some accounts you might need to meet requirements like adding numbers and symbols.";
+    
+        document.getElementById("finMess").innerHTML =finalmessage; 
       }
 
-
-        //function searches for the correct word using the generated code. 
-    
-        function match(number){
-
-          console.log(number); 
-          
-          for (i = 0; i < words.length; i++) {
-
-          var innerArray = words[i];
-          
-          if (innerArray[0]==number) {
-
-            if (innerArray[1].length<=3){
-              match(getNewRoll());
-              return;
-            }
-            console.log(innerArray);
-            matchingElements.push(innerArray[1]);
-            break;
-            } 
-          }     
-          
-          console.log(matchingElements);
-        }
-
-        //In case initials are used. matchInit() calls itself and getNewRoll() 
-        //until a word with the correct initial is received. 
-        function matchInit(number,index){
-
-          for (i = 0; i < words.length; i++) {
-
-          var innerArray = words[i];
-
-          if (innerArray[0]==number) {
-          var word=innerArray[1];
-          if (word.charAt(0) != initArr[index]){
-              matchInit(getNewRoll(),index);
-              return;
-           }
-        console.log(innerArray);
-        matchingElements.push(innerArray[1]);
-        break;
-        } 
-        }     
-        }
-
-
-    //Functions getNewRoll() In case a word is too small we roll dice again for a new code and return.  
-        
-    function getNewRoll(){
-
-      var diceArr=[];
-      for (j=0;j<numT;j++)
-          {
-            diceArr[j]=diceRoll();
-            console.log("Element "+(j+1)+" of word "+(i+1)+" is: "+diceArr[j]);
-          }
-      var diceAsString = diceArr.join('');
-
-      return diceAsString; 
-    }
-        
-
-     
-      //Function diceRoll returns a random number between 1 and 6. 
-     function diceRoll(){
-        return 1 + Math.floor(Math.random() * 6);
-     }
-        
+    //Loads start function when page is loaded.
      window.addEventListener("load",start,false);
---
+++ generate.js
@@ -0,0 +1,195 @@
+//File: generate.js
+//Project: Intro Cybersecurity Project Dice Ware Passphrase Generator. 
+//Author: Jose Ron Coka
+//History: Version 2.0 March 12 2026
+//This file contains the logic for generating the passphrase and the helper functions.
+//It exports the main functions to be used in app.js.
+    
+      //Global variables 
+      var pLen;
+      var numT;
+      var initL;
+      var initArr=[];
+      var words = []; // array to store the words
+      var matchingElements = []; //array to store the matching words to the dice throws
+
+      
+      //Main function
+      //Generates and returns the passphrase with the help of helper functions. 
+      //Receives word array, number of throws required for wordlist and the passphrase length.
+      export function generateFunc( wordsTrans, numTt, pLent, ){
+
+        //Assigning the passed parameters to global variables.
+        pLen=pLent;
+        numT=numTt;
+        words = wordsTrans; // array populated
+        
+        var diceArr=new Array(numT); //Start dice array 
+        var passphrase = new Array(pLen); //start pashprase array
+        
+
+        //Two loops, one runs for every word, another runs the dice roll depending on the number required for the wordlist. Defautl is 5.
+        //Two arrays, one for passphrase another for each dice throw.
+        //After second loop runs the array with the dice throws is turned to a string and saved on the passphrase array. 
+        for (let i=0; i<pLen;i++)
+        {
+          for (let j=0;j<numT;j++)
+          {
+            diceArr[j]=diceRoll();
+            console.log("Element "+(j+1)+" of word "+(i+1)+" is: "+diceArr[j]);
+          }
+          var diceAsString = diceArr.join(''); // Results in a string
+          //Populate the passphrase array with the generated number codes.
+          passphrase[i]=diceAsString; 
+          console.log(passphrase[i]);
+        } 
+
+        console.log(passphrase.length);
+
+        for (let x=0; x<=pLen; x++)
+        {
+          match(passphrase[x]);
+        }
+        
+        //Resulting Passphrase value is assigned to message. 
+        //matchingElements array contain the actual words that match the generated code.
+        var message="";
+        for (let y=0; y<pLen; y++)
+        {
+          message+=matchingElements[y]+ " ";
+        }
+
+        //Empty array in case user press button again. 
+        matchingElements=[];
+        
+        //Return the generated passphrase to the main function.
+        return message;
+      }
+
+      //Function to generate acronym passphrase
+      export function generateAcronym(wordsTrans, numTt, initLt,){
+        
+        //Assigning the passed parameters to global variables.
+        initL=initLt;//Extract input acronym word
+        initArr=initL.split(''); //Split the acronym into an array of initials.
+        pLen=initArr.length;//Get passphrase length from the number of initials provided.
+        numT=numTt; //Required throws for wordlist. Default is 5.
+        words = wordsTrans; // wordlist array populated
+
+        var diceArr=new Array(numT); //Start dice array 
+        var passphrase = new Array(pLen); //start pashprase array
+        
+
+        //Two loops, one runs for every word, another runs the dice roll depending on the number required for the wordlist. Defautl is 5.
+        //Two arrays, one for passphrase another for each dice throw.
+        //After second loop runs the array with the dice throws is turned to a string and saved on the passphrase array. 
+        for (let i=0; i<pLen;i++)
+        {
+          for (let j=0;j<numT;j++)
+          {
+            diceArr[j]=diceRoll();
+            console.log("Element "+(j+1)+" of word "+(i+1)+" is: "+diceArr[j]);
+          }
+          var diceAsString = diceArr.join(''); // Results in a string
+          //Populate the passphrase array with the generated number codes.
+          passphrase[i]=diceAsString; 
+          console.log(passphrase[i]);
+        } 
+
+        console.log(passphrase.length);
+
+        //Since acronym we call matchInit
+        //It will keep rolling until it finds a code and word that matches the initial.
+        for (let x=0; x<=pLen; x++)
+        {
+          matchInit(passphrase[x],x);
+        }
+
+        //Get the resulting Passphrase value.
+        var message="";
+        for (let y=0; y<pLen; y++)
+        {
+          message+=matchingElements[y]+ " ";
+        }
+
+        //Empty array in case user press button again. 
+        matchingElements=[];
+        
+        //Return the generated passphrase to the main function.
+        return message;
+
+      }
+
+
+        //function searches for the correct word using the generated code. 
+    
+        function match(number){
+
+          console.log(number); 
+          
+          for (let i = 0; i < words.length; i++) {
+
+          var innerArray = words[i];
+          
+          if (innerArray[0]==number) {
+
+            if (innerArray[1].length<=3){
+              match(getNewRoll());
+              return;
+            }
+            console.log(innerArray);
+            matchingElements.push(innerArray[1]);
+            break;
+            } 
+          }     
+          
+          console.log(matchingElements);
+        }
+
+        //Like match() it looks for the word that matches the generated code 
+        //but also checks if the word starts with the correct initial. 
+        //If not it rolls again until it finds a match.
+        function matchInit(number,index){
+
+            console.log("Looking for a word that starts with "+initArr[index]);
+
+          for (let i = 0; i < words.length; i++) {
+
+          var innerArray = words[i];
+
+          if (innerArray[0]==number) {
+          var word=innerArray[1];
+          if (word.charAt(0) != initArr[index]){
+              console.log("Word "+word+" does not start with "+initArr[index]+". Rolling again.");
+              matchInit(getNewRoll(),index);
+              return;
+           }
+        console.log(innerArray);
+        matchingElements.push(innerArray[1]);
+        break;
+        } 
+        }     
+        }
+
+
+    //Functions getNewRoll() In case a word is too small we roll dice again for a new code and return.  
+        
+    function getNewRoll(){
+
+      var diceArr=[];
+      for (let j=0;j<numT;j++)
+          {
+            diceArr[j]=diceRoll();
+            //console.log("Element "+(j+1)+" of word "+(i+1)+" is: "+diceArr[j]);
+          }
+      var diceAsString = diceArr.join('');
+
+      return diceAsString; 
+    }
+        
+
+     
+      //Function diceRoll returns a random number between 1 and 6. 
+     function diceRoll(){
+        return 1 + Math.floor(Math.random() * 6);
+     }

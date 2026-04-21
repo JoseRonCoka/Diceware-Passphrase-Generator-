bfe89a82e6f8b23e855b66c27ef185d1a4bc042d
josel <joseluisroncoka@hotmail.com>
2026-04-21 18:34:50 -0500

    Final check for functionality, deleted unused code, added comments, commented out most test console.logs

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
 //File: app.js
 //Project: Intro Cybersecurity Project Dice Ware Passphrase Generator.
 //Author: Jose Ron Coka
-//History: Version 2.0 March 11 2026
-//This should be the main javascript file, handles all the logic of navigation
-//The passphrase generating logic must be moved to a different file and imported here.
+//History: Version 2.0 April 21 2026
+//Main javascript file, handles all the logic of navigation and calls for helper generate module
 
 //Setting up global variables.
-var pLen;
-var numT;
-var initL;
-var initArr = [];
-
 const words = []; // array to store the words
-var matchingElements = []; //array to store the matching words to the dice throws
 
 //Import required functions from generate.js
 import { generateFunc } from "./generate.js";
@@ -27,16 +20,17 @@ function start() {
     fr.readAsText(fileInput.files[0]);
 
     fr.addEventListener("load", () => {
-      console.log(fr.result);
+      //Prints out results of the file read, to the console for testing purposes.
+      //console.log(fr.result);
 
       const lines = fr.result.split("\n");
-      //const lines = split.replace('\r','');
+
       // Loop through the lines and split each line by spaces
       for (const line of lines) {
         const wordPairs = line.split("\t"); // split the line by spaces
         words.push(wordPairs);
       }
-      console.log(words);
+      //console.log(words);
     });
   });
 
@@ -56,28 +50,29 @@ function start() {
 //Function Extracts input information and calls the generate function for DICEWARE PASSPHRASE.
 function extract() {
   //Takes the input from the HTML
-  numT = document.getElementById("throws").value;
-  console.log(numT);
+
+  //Extract required throw number
+  var numT = document.getElementById("throws").value;
+
+  //Extract required passphrase length
   var pLen = document.getElementById("lenP").value;
 
-  //Prints inputs to the html.
-  // document.getElementById("disLen").innerHTML ="Length of Passphrase: "+pLen+" Number of Dice Throws: "+numT;
-  console.log(pLen);
+  //Get the generated passphrase from the generate function, passing the required parameters.
   var message = generateFunc(words, numT, pLen);
 
-  if (message == "error"){
-    message = "No passphrase generated, please input a valid wordlist, refresh the page and try again.";
-     document.getElementById("result").innerHTML = message;
-
-      return;
+  //Sentinel, if the message returned is "error" it means that the wordlist was not valid and no passphrase was generated
+  if (message == "error") {
+    message =
+      "No passphrase generated, please input a valid wordlist, refresh the page and try again.";
+    document.getElementById("result").innerHTML = message;
+    return;
   }
 
-
-  //document.getElementById("test").innerHTML ="Passphrase Generated: " + message;
   document.getElementById("result").innerHTML = message;
 
   //Calculate Entropy entropy = log₂(pool_size) × word_count
   const entropy = Math.round(Math.log2(words.length) * pLen);
+
   document.getElementById("entropyResult").innerHTML =
     "Entropy of the generated passphrase: " + entropy + " bits.";
   document.getElementById("strength").innerHTML =
@@ -92,6 +87,8 @@ function extract() {
   finalmessage +=
     "<br> To use it for some online accounts you might need to add numbers and symbols to it, making the passphrase more secure.";
   document.getElementById("finalRec").innerHTML = finalmessage;
+
+  //Empty and update final message with instructions to run again.
   document.getElementById("finalMess").textContent = "";
   document.getElementById("finalMess").innerHTML =
     "<br> TO RUN AGAIN WITH SAME WORDLIST: Repeat Steps 2 through 4. <br> TO RUN AGAIN WITH DIFFERENT WORDLIST: Reload the page and repeat steps 1 through 4. <br>";
@@ -102,40 +99,35 @@ function extractAcr() {
   //Takes the input from the HTML
   //Extract required throw number
   numT = document.getElementById("throws").value;
-  console.log(numT);
+  
   //Extract word with initials for passphrase
-  //document.getElementById("initials").maxlength = pLen;
   var initL = document.getElementById("initials").value;
   var initArr = initL.split("");
   var pLen = initArr.length;
 
-  //Prints inputs to the html.
-  // document.getElementById("disLen").innerHTML ="Length of Passphrase: "+pLen+" Number of Dice Throws: "+numT;
-  console.log(pLen);
-  console.log(initArr);
+  //Get the generated passphrase from the generate function, passing the required parameters.
   var message = generateAcronym(words, numT, initL);
- 
-  if (message == "error"){
-    message = "No passphrase generated, please input a valid wordlist, refresh the page and try again.";
-     document.getElementById("result").innerHTML = message;
-     return;
+
+  //Sentinel, if the message returned is "error" it means that the wordlist was not valid and no passphrase was generated
+  if (message == "error") {
+    message =
+      "No passphrase generated, please input a valid wordlist, refresh the page and try again.";
+    document.getElementById("result").innerHTML = message;
+    return;
   }
 
-
-  
   document.getElementById("result").innerHTML = message;
 
+  //Calculate Entropy for acronym passphrase, using the pool sizes of each letter.
   const poolSizes = buildLetterPoolSizes(words);
   const acronymEntropy = calculateAcronymEntropy(initL, poolSizes);
 
-
   //Calculate Entropy entropy = log₂(pool_size) × word_count
-  //const entropy = Math.round(Math.log2(words.length)*pLen);
+ 
   document.getElementById("entropyResult").innerHTML =
     "Entropy of the generated passphrase: " + acronymEntropy + " bits.";
   document.getElementById("strength").innerHTML =
-    "Strength of the generated passphrase: " +
-    strengthCalc(acronymEntropy);
+    "Strength of the generated passphrase: " + strengthCalc(acronymEntropy);
 
   //Final message is printed
   var finalmessage = "If you are happy with the generated passphrase, ";
@@ -164,7 +156,7 @@ function buildLetterPoolSizes(wordlist) {
     //Each item in an obj is stored as a key value pair, meaning we cannot get repeats since each word gets assigned to its starting letter key.
     poolSizes[letter] = (poolSizes[letter] || 0) + 1; //Safeguard in case there are letters with no words, or operator will assign 0 to them.
   }
-  console.log(poolSizes);
+  
   return poolSizes;
 }
 
@@ -172,7 +164,7 @@ function buildLetterPoolSizes(wordlist) {
 function calculateAcronymEntropy(seedWord, poolSizes) {
   const letters = seedWord.toLowerCase().split("");
   return Math.round(
-    //Resuce method iterates through the letters of the acronym, for each letter it gets the pool size from the poolSizes object and calculates log2 of it.
+    //Reduce method iterates through the letters of the acronym, for each letter it gets the pool size from the poolSizes object and calculates log2 of it.
     //Therefore it is adding the entropy of each individual letter to get the total entropy.
     letters.reduce((total, letter) => {
       const poolSize = poolSizes[letter] || 1;
